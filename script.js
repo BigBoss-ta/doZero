ea46fdf3f8221dd53149922536e945a2ae66f1a3
BigBoss-ta-2213 <isack_santana@estudante.sesisenai.org.br>
2026-03-17 08:34:38 -0300

    Add initial HTML structure and JavaScript functionality for number summation

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("Hello World!")
+
+let nome = "Isack";
+let idade = 17;
+let peso = 70;
+let sabe = true;
+
+console.log("Meu nome é " + nome)
+
+function somarNumeros() {
+    if soma = 0;
+    let continuar = true
+}
+
+while(continuar) {
+    let numero = parseFloat/prompt
+
+    if(isNaN(numero))(
+        alert("Por favor, digite um numero valido")
+    )
+    else{
+        soma += numero
+    }
+}
+
