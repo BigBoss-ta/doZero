57030ecbf0b824236614dafe38b3d24c580df932
BigBoss-ta-2213 <isack_santana@estudante.sesisenai.org.br>
2026-03-19 08:37:49 -0300

    feat: add number organization functionality with user input

---
+++ script-04.js
@@ -0,0 +1,22 @@
+function numberOrganizer() {
+    let n1 = parseFloat(prompt("Digite o primeiro número:"));
+    let n2 = parseFloat(prompt("Digite o segundo número:"));
+    let n3 = parseFloat(prompt("Digite o terceiro número:"));
+    let n4 = parseFloat(prompt("Digite o quarto número:"));
+    let buffer = 0;
+    let numbers = [n1, n2, n3, n4];
+
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4 - i - 1; j++) {
+            if (numbers[j] > numbers[j + 1]) {
+                buffer = numbers[j];
+                numbers[j] = numbers[j + 1];
+                numbers[j + 1] = buffer;
+            }
+        }
+    }
+    alert("Os números organizados em ordem crescente são: " + numbers);
+    console.log("Os números organizados em ordem crescente são: " + numbers);
+}
+
+numberOrganizer();
